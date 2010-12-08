be1f72d96646ae75ffd062a3d28d50668fd33b9e
Jesse Rivera <jesse4th@gmail.com>
2010-12-07 19:33:05 -0600

    Styled the admin section for the food

--- vendor/plugins/foods/app/models/food.rb
+++ vendor/plugins/foods/app/models/food.rb
@@ -6,6 +6,7 @@ class Food < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name
 
-
+  named_scope :published, :conditions => { :published => true }, :order => "position ASC"
+  named_scope :unpublished, :conditions => { :published => false }
 
 end
