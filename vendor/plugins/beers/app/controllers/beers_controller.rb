f099b3c950d7ddc80d9456ef5d189be984c6ad14
Will Barrett <william.barrett@tallgreentree.com>
2011-01-10 21:30:42 -0600

    Went from dynamic to static major beer categories.

--- vendor/plugins/beers/app/controllers/beers_controller.rb
+++ vendor/plugins/beers/app/controllers/beers_controller.rb
@@ -1,11 +1,13 @@
 class BeersController < ApplicationController
   include ActionView::Helpers::NumberHelper
-  before_filter :find_beer_categories
+  # before_filter :find_beer_categories
   before_filter :find_page
 
   def index
     # you can use meta fields from your model instead (e.g. browser_title)
     # by swapping @page for @beer in the line below:
+    @drafts = Beer.drafts.live
+    @bottles = Beer.bottles.live
     present(@page)
   end
 
@@ -22,9 +24,9 @@ class BeersController < ApplicationController
 
 protected
 
-  def find_beer_categories
-    @beer_categories = BeerCategory.all
-  end
+  # def find_beer_categories
+  #   @beer_categories = BeerCategory.all
+  # end
 
   def find_page
     @page = Page.find_by_link_url("/beers")
