8645bb5bee5c5358735c9b76ca9c4e56c4422d8b
Digital Traffic <digitaltraffic@digitraf.local>
2011-01-19 19:21:11 -0600

    Fixed sidebar filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@
 # You can extend refinery with your own functions here and they will likely not get overriden in an update.
 
 class ApplicationController < Refinery::ApplicationController
+  before_filter :set_show_sidebar
+  
+  def set_show_sidebar
+    @show_sidebar = true
+  end
 end
