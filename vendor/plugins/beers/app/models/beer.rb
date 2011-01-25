b6fe27cc0ee5b869a965b238f93768955c9ec430
Will <wbarrett@sccadv.com>
2011-01-25 13:47:31 -0600

    Changing = Haymarket test to LIKE Haymaret.

--- vendor/plugins/beers/app/models/beer.rb
+++ vendor/plugins/beers/app/models/beer.rb
@@ -4,7 +4,7 @@ class Beer < ActiveRecord::Base
 
   validates_presence_of :name
   validates_uniqueness_of :name
-	validates_presence_of :beer_category_id
+	# validates_presence_of :beer_category_id
 
   has_many :beer_pricings
   has_many :beer_sizes, :through => :beer_pricings
@@ -17,8 +17,8 @@ class Beer < ActiveRecord::Base
   named_scope :pending, :conditions => {:published => false}
   named_scope :bottles, :conditions => ["bottle_price IS NOT NULL"]
   named_scope :drafts, :conditions => ["draft_price_4oz IS NOT NULL OR draft_price_12oz IS NOT NULL OR draft_price_16oz IS NOT NULL or draft_price_20oz IS NOT NULL"]
-  named_scope :haymarket, :conditions => {:brewery => 'Haymarket'}
-  named_scope :not_haymarket, :conditions => ["`brewery` != 'Haymarket'"]
+  named_scope :haymarket, :conditions => ['`brewery` LIKE ?', '%Haymarket%']
+  named_scope :not_haymarket, :conditions => ['`brewery` NOT LIKE ?', '%Haymarket%']
   named_scope :by_name, :order => :name
   named_scope :unpriced, :conditions => {:bottle_price => nil, :draft_price_4oz => nil, :draft_price_12oz => nil, :draft_price_16oz => nil, :draft_price_20oz => nil}
 
