9d285ee2c667815ca70a1398afc31064271687bb
Digital Traffic <digitaltraffic@digitraf.local>
2011-03-28 20:47:24 -0500

    accolades section, facebook sidebar widget

--- app/controllers/accolades_controller.rb
+++ app/controllers/accolades_controller.rb
@@ -1,7 +1,8 @@
 class AccoladesController < ApplicationController
   def index
-   @awards = Award.published
+   @awards = Award.published(:order => 'year')
    @testimonials = Testimonial.published
    @presses = Press.published
+   @award_year = @awards.group_by { |a| a.year.year }
   end
 end
