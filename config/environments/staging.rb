100a99b5e07d21477413a7647b6403dd06d1b6e0
Mike Doyle <mdoyle@sccadv.com>
2014-02-25 12:31:11 -0600

    reset

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -26,9 +26,3 @@ config.log_level = :debug
 # instead of the default file system for resources and images
 # Make sure to your bucket info is correct in amazon_s3.yml
 Refinery.s3_backend = !(ENV['S3_KEY'].nil? || ENV['S3_SECRET'].nil?)
-config.action_mailer.perform_deliveries = true
-config.action_mailer.delivery_method = :sendmail
-config.action_mailer.sendmail_settings = {
-  :location => '/usr/sbin/sendmail',
-  :arguments => '-i -t'
-}
