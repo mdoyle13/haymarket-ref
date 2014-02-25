fdab9d0cbfed905efbf168018a23bd591fa3175d
Mike Doyle <mdoyle@sccadv.com>
2014-02-25 11:11:59 -0600

    testing mailer settings

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -26,3 +26,6 @@ config.log_level = :debug
 # instead of the default file system for resources and images
 # Make sure to your bucket info is correct in amazon_s3.yml
 Refinery.s3_backend = !(ENV['S3_KEY'].nil? || ENV['S3_SECRET'].nil?)
+
+config.action_mailer.delivery_method = :sendmail
+config.action_mailer.raise_delivery_errors = true
