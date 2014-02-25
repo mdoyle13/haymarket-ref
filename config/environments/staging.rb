2df981c7358513112dac276e173c2fedd2bfc388
Mike Doyle <mdoyle@sccadv.com>
2014-02-25 10:10:30 -0600

    trying to get email to deliver

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -16,11 +16,11 @@ config.action_controller.perform_caching             = true
 
 # Disable delivery errors, bad email addresses will be ignored
 config.action_mailer.raise_delivery_errors = true
-
+config.action_mailer.perform_deliveries = true
 # Log level is set to :info by default which is the minimum to let you know what
 # is going on but without being chatty and therefore slow.
 config.log_level = :debug
-config.action_mailer.delivery_method = :sendmail
+
 # When true will use Amazon's Simple Storage Service on your production machine
 # instead of the default file system for resources and images
 # Make sure to your bucket info is correct in amazon_s3.yml
