bada04a2f1ab25a806aef6278fd19e9886697741
Mike Doyle <mdoyle@sccadv.com>
2014-02-25 10:39:55 -0600

    sendmail not working

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -15,8 +15,9 @@ config.action_controller.perform_caching             = true
 # config.action_controller.asset_host                  = "http://assets.example.com"
 
 # Disable delivery errors, bad email addresses will be ignored
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.perform_deliveries = true
+# config.action_mailer.raise_delivery_errors = true
+# config.action_mailer.perform_deliveries = true
+
 # Log level is set to :info by default which is the minimum to let you know what
 # is going on but without being chatty and therefore slow.
 config.log_level = :debug
@@ -25,3 +26,6 @@ config.log_level = :debug
 # instead of the default file system for resources and images
 # Make sure to your bucket info is correct in amazon_s3.yml
 Refinery.s3_backend = !(ENV['S3_KEY'].nil? || ENV['S3_SECRET'].nil?)
+
+config.action_mailer.delivery_method = :sendmail
+config.action_mailer.raise_delivery_errors = true
